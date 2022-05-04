91a53732ecc555dde2dbda84575e766d232c3706
Vitaliy <komissarov.v.2003@mail.ru>
2022-05-04 13:09:54 +0300

    second commit

--- src/find_sum.cpp
+++ src/find_sum.cpp
@@ -3,10 +3,26 @@
 namespace assignment {
 
   std::optional<std::pair<int, int>> find_sum(const std::vector<int>& data, int sum) {
+    if (static_cast<int>(data.size()) == 1) {
+      return std::nullopt;
+    }
+    int left = 0;
+    int right = static_cast<int>(data.size())-1;
+    while (left < right) {
+      if ((data[left] + data[right]) == sum) {
+        return std::make_pair(left,right);
+      }
+      if ((data[left] + data[right]) > sum) {
+        right--;
+      }
+      if ((data[left] + data[right]) < sum) {
+        left++;
+      }
+    }
+    return std::nullopt;
 
     // Tips: для создания пары (pair) используйте функцию std::make_pair
 
-    return std::nullopt;
   }
 
 }  // namespace assignment
